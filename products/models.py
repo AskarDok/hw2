85b962e40b56c3a7c19161b5f86069b0a0971076
AskarDok <dokturbekovaskar@gmail.com>
2021-07-10 19:59:20 +0600

    third commit

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+        name = models.CharField(max_length=30)
+
+        def __str__(self):
+            return self.name
+
+class Product(models.Model):
+    title =       models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price =       models.FloatField()
+    category =    models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
